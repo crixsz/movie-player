87e1cbe19b09376ed2840f2e45fe6423e238141b
crixsz <xenonzox@gmail.com>
2025-09-11 22:55:36 +0800

    ui: make fullscren button bigger and 1.5sec to hide control mouseleave

--- src/VideoPlayer.jsx
+++ src/VideoPlayer.jsx
@@ -205,7 +205,7 @@ const VideoPlayer = ({ src, subtitles }) => {
     if (isPlaying) {
       controlsTimeoutRef.current = setTimeout(() => {
         setIsControlsVisible(false);
-      }, 2000);
+      }, 1500);
     }
   };
 
@@ -254,12 +254,12 @@ const VideoPlayer = ({ src, subtitles }) => {
     </svg>
   );
   const FullscreenEnterIcon = () => (
-    <svg className="w-6 h-6" viewBox="0 0 24 24" fill="currentColor">
+    <svg className="w-8 h-8" viewBox="0 0 24 24" fill="currentColor">
       <path d="M7 14H5v5h5v-2H7v-3zm-2-4h2V7h3V5H5v5zm12 7h-3v2h5v-5h-2v3zM14 5v2h3v3h2V5h-5z" />
     </svg>
   );
   const FullscreenExitIcon = () => (
-    <svg className="w-6 h-6" viewBox="0 0 24 24" fill="currentColor">
+    <svg className="w-8 h-8" viewBox="0 0 24 24" fill="currentColor">
       <path d="M5 16h3v3h2v-5H5v2zm3-8H5v2h5V5H8v3zm6 11h2v-3h3v-2h-5v5zm2-11V5h-2v5h5V8h-3z" />
     </svg>
   );
